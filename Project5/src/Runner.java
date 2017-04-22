ca4c1b2f2568e5dd94d641323dda547aed3b58de
drew <dr5801@cs.ship.edu>
2017-04-22 15:30:44 -0400

    finished basic example of weakreference with a string

--- Project5/src/Runner.java
+++ Project5/src/Runner.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,5 +13,28 @@ public class Runner
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		basicExampleOfWeakReference();
+		exampleWithWeakHashMap();
+	}
+
+	private static void exampleWithWeakHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * basic example of using a weakreference object
+	 */
+	private static void basicExampleOfWeakReference() 
+	{
+		WeakReference<String> basicString = new WeakReference<String>(new String("WeakReferenced String"));
+		
+		System.out.println("WeakReference String -> : " + basicString.get());
+		while(basicString.get() != null)
+		{
+			ArrayList<Integer> expensiveObject = new ArrayList<Integer>(10);
+		}
+		
+		System.out.println("Garbage collection occurred, WeakReference String -> : " + basicString.get());
 	}
 }
