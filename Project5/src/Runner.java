f24dc8a96547c09ed9cdaff060cd7bd100063a4f
drew <dr5801@cs.ship.edu>
2017-04-22 15:23:46 -0400

    re-commit source

---
+++ Project5/src/Runner.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.lang.ref.WeakReference;
+
+/**
+ *
+ * @author Drew Rife and Jordan Long
+ *
+ */
+public class Runner
+{
+	public static WeakReference<String> nameOfList;
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+	}
+}
